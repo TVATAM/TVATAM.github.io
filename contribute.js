b1142156a811f7d09757e4be03f1c31158c0aa11
TVATAM <4334@pierson.nl>
2015-12-09 21:45:02 +0100

    Bookstores

---
+++ contribute.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">var zlpuo2410iq20m;(function(d, t) {
+	var s = d.createElement(t), options = {
+	'userName':'tvatam',
+'formHash':'zlpuo2410iq20m',
+'autoResize':true,
+'height':'375',
+'async':true,
+'host':'wufoo.com',
+'header':'show',
+'ssl':true};
+s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+s.onload = s.onreadystatechange = function() {
+var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+try { zlpuo2410iq20m = new WufooForm();zlpuo2410iq20m.initialize(options);zlpuo2410iq20m.display(); } catch (e) {}};
+var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})(document, 'script');</script>
